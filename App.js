b09b2f5539fdc06a7ebaab32f2edcd0d174ef55a
Mayela Bautista <maryury_bautista@hotmail.com>
2019-12-05 18:16:39 -0600

    segundo commit

--- App.js
+++ App.js
@@ -18,3 +18,63 @@ class HomeScreen extends React.Component {
     );
   }
 }
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "stretch",  justifyContent: "space-between"}}>
+        
+        <ImageBackground source={require('./src/imgs/background.jpg')} style={{width: '100%', height: '100%', flex: 1, alignItems: "stretch",  justifyContent: "space-between"}}>
+        <Text style= {styles.submitButtonText}>Mis dinosaurios</Text>
+        <Button 
+          title="Tiranosaurio"
+          //onPress={() => this.props.navigation.push('Tiranosaurio')}
+          onPress={() => this.props.navigation.navigate('Tiranosaurio')}
+         />
+        <Button
+          title="Ankilosaurio"
+          onPress={() => this.props.navigation.navigate('Ankilosaurio')}
+        />
+        <Button
+          title="Espinosaurio"
+          //onPress={() => this.props.navigation.goBack()}
+          onPress={() => this.props.navigation.navigate('Espinosaurio')}
+        />
+        <Button
+          title="Triceratops"
+          //onPress={() => this.props.navigation.push('Tiranosaurio')}
+          onPress={() => this.props.navigation.navigate('Triceratops')}
+        />
+        <Button
+          title="Carcharadontosaurio"
+          onPress={() => this.props.navigation.navigate('Carcharadontosaurio')}
+        />
+        <Button
+          title="Velociraptor"
+          //onPress={() => this.props.navigation.goBack()}
+          onPress={() => this.props.navigation.navigate('Velociraptor')}
+        />
+        <Button
+          title="Mutaburrasaurio"
+          //onPress={() => this.props.navigation.goBack()}
+          onPress={() => this.props.navigation.navigate('Mutaburrasaurio')}
+        />
+        <Button
+          title="Carnotauro"
+          //onPress={() => this.props.navigation.push('Tiranosaurio')}
+          onPress={() => this.props.navigation.navigate('Carnotauro')}
+        />
+        <Button
+          title="Estegosaurio"
+          onPress={() => this.props.navigation.navigate('Estegosaurio')}
+        />
+        <Button
+          title="Gallimimus"
+          //onPress={() => this.props.navigation.goBack()}
+          onPress={() => this.props.navigation.navigate('Gallimimus')}
+        />
+         </ImageBackground>
+      </View>
+    );
+  }
+}
